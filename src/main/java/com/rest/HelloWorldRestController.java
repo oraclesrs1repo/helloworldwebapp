2d9d9cc7e81149da1c2bdd305deed11c2ac67e53
anantoracle <oraclesrs1@gmail.com>
2017-07-20 12:33:37 +0530

    committed project

---
+++ src/main/java/com/rest/HelloWorldRestController.java
@@ -0,0 +1,24 @@
+package com.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+
+@Path("/rest")
+public class HelloWorldRestController {
+	
+	@GET
+	@Path("/hello")
+	public Response sayHello(){
+		return Response.status(200).entity("Hello Welcome to Tavant").build();
+	}
+	
+	
+	@GET
+	@Path("/bye")
+	public Response sayBye(){
+		return Response.status(200).entity("Good Bye").build();
+	}
+
+}
